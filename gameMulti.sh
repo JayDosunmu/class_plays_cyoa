fa1836b0f158370fb3341399bd9df2145adb7a2d
Taslim J Dosunmu <jaydosunmu@Taslims-MBP.gsu.edu>
2017-11-30 12:13:56 -0500

    Added a bunch of events, bug fixes and improvements

--- gameMulti.sh
+++ gameMulti.sh
@@ -8,45 +8,44 @@
 
 #initial event
 event=1
-name=0
+victory=0
 death=0
 continue=1
 file=commands
 
 while [ $continue -eq 1 ]; ## main loop, to allow for multiple endings
 do
-
+#initial clear
+clear
 #while loop goes until specific events are completed, can be modified for specific event numbers to terminate- i.e. final events can add 100 to event to terminate loop
-while [ $event -lt 4 ]; 
+while [ $event -lt 26 ]; 
 do
+	#victory handler to change initial event
+	if [ $event -eq 21 ]
+	then
+		victory=1
+	fi
+	
+	if [ $event -eq 1 -a $victory -eq 1 ]
+	then
+		event=22
+	fi
 
 	#Use cat to print out event and choice. 
 	#Can add comments in Choice files after 3rd :
-	cat Event/Event$event
-	if [ $event -eq 6 -a $name -eq 1 ] #checking if the name is known
-        then
-                cat Choice/Choice$event | awk -F: '{ print $1 "." $5 }'
-        else
-                cat Choice/Choice$event | awk -F: '{ print $1 "." $2 }'
-        fi
+	cat Event/Event$event | fmt
+        cat Choice/Choice$event | awk -F: '{ print $1 $2 }'
 
-
-	# Death and victory events
-	if [ $event -eq 70 -o $event -eq 50 -o $event -eq 30 ]
+	# death or victory handler
+	if [ $event -eq 8 -o $event -eq 10 -o $event -eq 12 -o $event -eq 14 -o $event -eq 16 -o $event -eq 21 -o $event -eq 23 ]
 	then	
-		death=1
+		death=`expr $death + 1` 
 		break
-	elif [ $event = 90 ] ##### winning event
+	elif [ $event -eq 25 ] ##### winning event
 	then
 		continue=0
 		break
 	fi
-
-	###### 15 is a placeholder, whichever event reveals the man in the red hat's name.
-	if [ $event -eq 15 ]
-        then
-                name=1
-        fi
 	
 	#Single line countdown
 	#From https://serverfault.com/questions/532559/bash-script-count-down-5-minutes-display-on-single-line
@@ -59,6 +58,10 @@ do
 		: $((secs--))
 	done
 
+	# check for existance of B and C as options
+	Bexist=`cat Choice/Choice$event | awk -F: ' NR == 2 { print $4 }'`
+	Cexist=`cat Choice/Choice$event | awk -F: ' NR == 3 { print $4 }'`
+
 	#Logic for choice:
 	#A, B, and C grab the number of votes each got from the server text file.
 	A=`cat "$file" | grep 'A' | wc -l`
@@ -67,8 +70,15 @@ do
 
 	echo “Votes total:”
 	echo A: $A
-	echo B: $B
-	echo C: $C
+	if [ $Bexist -eq 1 ]
+	then
+		echo B: $B
+	fi
+	
+	if [ $Cexist -eq 1 ]
+	then
+		echo C: $C
+	fi
 
 	#Set temp to add to event modifier, highest count will decide where temp reads from
 	# temp reads from the correct choice file and returns the associated destination operator to add to event
@@ -76,11 +86,11 @@ do
 	then
 		temp=`cat Choice/Choice$event | awk -F: ' NR == 1 { print $3 }'` #choice=A
 		echo “A wins”
-	elif [ $B -ge $A -a $B -ge $C ]
+	elif [ $B -ge $A -a $B -ge $C -a $Bexist -eq 1 ]
 	then
 		temp=`cat Choice/Choice$event | awk -F: ' NR == 2 { print $3 }'` #choice=B
 		echo “B wins”
-	elif [ $C -ge $A -a $C -ge $B ]
+	elif [ $C -ge $A -a $C -ge $B -a $Cexist -eq 1 ]
 	then
 		temp=`cat Choice/Choice$event | awk -F: ' NR == 3 { print $3 }'` #choice=C
 		echo “C wins”
@@ -89,13 +99,8 @@ do
 		temp=0
 	fi
 
-	# add temp to event to proceed to next event and choice
-	if [ $event -eq 6 -a $name -eq 1 ]  ## test if name is figured out
-	then	
-		event=60		#########placeholder at 60
-	else
-		event=`expr $event + $temp`
-	fi
+	# Add temp to event
+	event=`expr $event + $temp`
 
 	# To clear test.txt for running program, remove comment on next line
 	>"$file"
@@ -115,25 +120,24 @@ do
 	clear
 done
 
-if [ $death -eq 1 ]
+if [ $continue -eq 1 ]
 then
 	sleep 10
-	echo Congratulations, you have died. 
-	sleep 2
-	echo But there is probably more to see, so let's try again!
-	sleep 2
-	echo Good luck!
-	sleep 2
-
+	echo "Congratulations, you have died $death times now." 
+	sleep 3
+	echo "But there is probably more to see, so let's try again!"
+	sleep 3
+	echo "Good luck!"
+	sleep 3
 	event=1
-	death=0
 else
 	sleep 10
-	echo Congratulations! You did it. You won.
+	echo "Congratulations! You did it. You won."
 	sleep 2
-	echo It's finally over. Thanks for playing!
+	echo "It's finally over. Thanks for playing!"
 	sleep 2
+	echo "You you got your guy killed $death times!"
 fi
-	
+
 done
 
